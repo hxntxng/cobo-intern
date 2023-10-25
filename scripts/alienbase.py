2548c184bf5b8e3bd350c2143becbe6b2c90d40e
Hanting Li <hanting6@gmail.com>
2023-10-24 22:03:10 -0700

    change to brownie format

--- scripts/alienbase.py
+++ scripts/alienbase.py
@@ -11,40 +11,40 @@ def a_deposit(w3, acct, val):
     nonce = w3.eth.get_transaction_count(acct.address)
     cbeth_token_address = '0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22'
     cbeth_token_abi = abi.alienbase_cbeth_token_abi
-    cbeth_token_contract = w3.eth.contract(address = cbeth_token_address, abi = cbeth_token_abi)
+    cbeth_token_contract = Contract.from_abi("UpgradeableOptimismMintableERC20", address = cbeth_token_address, abi = cbeth_token_abi)
     uniswap_router_address = '0x8c1A3cF8f83074169FE5D7aD50B978e1cD6b37c7'
     unlim = 115792089237316195423570985008687907853269984665640564039457584007913129639935
-    approve_cbeth_token_txn = cbeth_token_contract.functions.approve(uniswap_router_address, unlim).build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce,})
-    approve_cbeth_token_signtxn = w3.eth.account.sign_transaction(approve_cbeth_token_txn, private_key = acct.private_key)
-    w3.eth.send_raw_transaction(approve_cbeth_token_signtxn.rawTransaction)
+    cbeth_token_contract.approve(uniswap_router_address, unlim, {'from': acct})
+    # approve_cbeth_token_signtxn = w3.eth.account.sign_transaction(approve_cbeth_token_txn, private_key = acct.private_key)
+    # w3.eth.send_raw_transaction(approve_cbeth_token_signtxn.rawTransaction)
     # breakpoint()
     uniswap_router_abi = abi.alienbase_uniswap_router_abi
-    uniswap_router_contract = w3.eth.contract(address = uniswap_router_address, abi = uniswap_router_abi)
+    uniswap_router_contract = Contract.from_abi("UniswapV2Router02", address = uniswap_router_address, abi = uniswap_router_abi)
     acct_address = acct.address
     # print(dir(add_contract.functions))
     token_amt = 9547905041126
     token_min = 9500165515920
     eth_min = 9950000000000
     deadline = int(time.time()) + 1800
-    addLiquidity_uniswap_txn = uniswap_router_contract.functions.addLiquidityETH(cbeth_token_address, token_amt, token_min, eth_min, acct_address, deadline).build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce, 'value': w3.toWei(f'{val/10000000}', 'gwei')})
-    addLiquidity_uniswap_signtxn = w3.eth.account.sign_transaction(addLiquidity_uniswap_txn, private_key = acct.private_key)
-    w3.eth.send_raw_transaction(addLiquidity_uniswap_signtxn.rawTransaction)
+    uniswap_router_contract.addLiquidityETH(cbeth_token_address, token_amt, token_min, eth_min, acct_address, deadline, {'from': acct, 'value': acct.balance/2})
+    # addLiquidity_uniswap_signtxn = w3.eth.account.sign_transaction(addLiquidity_uniswap_txn, private_key = acct.private_key)
+    # w3.eth.send_raw_transaction(addLiquidity_uniswap_signtxn.rawTransaction)
     # breakpoint()
     uniswap_pair_address = '0x9BB646BF0F4Da44bfaF3d899e774DE065731EDFe'
     uniswap_pair_abi = abi.alienbase_uniswap_pair_abi
-    uniswap_pair_contract = w3.eth.contract(address = uniswap_pair_address, abi = uniswap_pair_abi)
+    uniswap_pair_contract = Contract.from_abi("UniswapV2Pair", address = uniswap_pair_address, abi = uniswap_pair_abi)
     based_distributor_address = '0x52eaeCAC2402633d98b95213d0b473E069D86590'
-    approve_uniswap_txn = uniswap_pair_contract.functions.approve(based_distributor_address, val).build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce,})
-    approve_uniswap_signtxn = w3.eth.account.sign_transaction(approve_uniswap_txn, private_key = acct.private_key)
-    w3.eth.send_raw_transaction(approve_uniswap_signtxn.rawTransaction)
+    uniswap_pair_contract.approve(based_distributor_address, val, {'from': acct})
+    # approve_uniswap_signtxn = w3.eth.account.sign_transaction(approve_uniswap_txn, private_key = acct.private_key)
+    # w3.eth.send_raw_transaction(approve_uniswap_signtxn.rawTransaction)
     # breakpoint()
     based_distributor_abi = abi.alienbase_based_distributor_abi
-    based_distributor_contract = w3.eth.contract(address = based_distributor_address, abi = based_distributor_abi)
+    based_distributor_contract = Contract.from_abi("BasedDistributorV2", address = based_distributor_address, abi = based_distributor_abi)
     pid = 6
     amount = 9764677045213 # not sure what this number is
-    deposit_distributor_txn = based_distributor_contract.functions.deposit(pid, val).build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce,})
-    deposit_distributor_signtxn = w3.eth.account.sign_transaction(deposit_distributor_txn, private_key = acct.private_key)
-    w3.eth.send_raw_transaction(deposit_distributor_signtxn.rawTransaction)
+    based_distributor_contract.deposit(pid, val, {"from": acct})
+    # deposit_distributor_signtxn = w3.eth.account.sign_transaction(deposit_distributor_txn, private_key = acct.private_key)
+    # w3.eth.send_raw_transaction(deposit_distributor_signtxn.rawTransaction)
     return
 
 def a_get_val(w3, acct):
@@ -52,39 +52,32 @@ def a_get_val(w3, acct):
     chainId = 1337
     based_distributor_address = '0x52eaeCAC2402633d98b95213d0b473E069D86590'
     based_distributor_abi = abi.alienbase_based_distributor_abi
-    based_distributor_contract = w3.eth.contract(address = based_distributor_address, abi = based_distributor_abi)
+    based_distributor_contract = Contract.from_abi("BasedDistributorV2", based_distributor_address, based_distributor_abi)
     pid = 6
     val = 0 # find value of alb
-    deposit_distributor_txn = based_distributor_contract.functions.deposit(pid, val).build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce,})
-    deposit_distributor_signtxn = w3.eth.account.sign_transaction(deposit_distributor_txn, private_key = acct.private_key)
-    # breakpoint()
-    w3.eth.send_raw_transaction(deposit_distributor_signtxn.rawTransaction)
-    # breakpoint()
-    val = based_distributor_contract.functions.userInfo(6, acct.address).call()[0]
-    # build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce,})
-    # val_signtxn = w3.eth.account.sign_transaction(val_txn, private_key = acct.private_key)
-    # print(val.call)
+    based_distributor_contract.deposit(pid, val, {'from': acct})
+    # .build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce,})
+    # deposit_distributor_signtxn = w3.eth.account.sign_transaction(deposit_distributor_txn, private_key = acct.private_key)
+    # w3.eth.send_raw_transaction(deposit_distributor_signtxn.rawTransaction)
+    val = based_distributor_contract.userInfo.call(6, acct.address, {'from': acct})[0]
+
     return val
 
 def a_withdraw(w3, acct):
     chainId=1337
     nonce = w3.eth.get_transaction_count(acct.address)
-    print(nonce)
     based_distributor_address = '0x52eaeCAC2402633d98b95213d0b473E069D86590'
     based_distributor_abi = abi.alienbase_based_distributor_abi
-    based_distributor_contract = w3.eth.contract(address = based_distributor_address, abi = based_distributor_abi)
+    based_distributor_contract = Contract.from_abi("BasedDistributorV2", address = based_distributor_address, abi = based_distributor_abi)
     pid = 6
     val = 0 # find value of alb
-    breakpoint()
-    deposit_distributor_txn = based_distributor_contract.functions.deposit(pid, val).build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce,})
-    deposit_distributor_signtxn = w3.eth.account.sign_transaction(deposit_distributor_txn, private_key = acct.private_key)
-    w3.eth.send_raw_transaction(deposit_distributor_signtxn.rawTransaction)
+    based_distributor_contract.functions.deposit(pid, val, {"from": acct})
+    # deposit_distributor_signtxn = w3.eth.account.sign_transaction(deposit_distributor_txn, private_key = acct.private_key)
+    # w3.eth.send_raw_transaction(deposit_distributor_signtxn.rawTransaction)
     val = a_get_val(w3, acct)
-    print(nonce)
-    breakpoint()
-    nonce = w3.eth.get_transaction_count(acct.address)
-    withdraw_distributor_txn = based_distributor_contract.functions.withdraw(6, val).build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce,})
-    withdraw_distributor_signtxn = w3.eth.account.sign_transaction(withdraw_distributor_txn, private_key = acct.private_key)
-    w3.eth.send_raw_transaction(withdraw_distributor_signtxn.rawTransaction)
+    # breakpoint()
+    based_distributor_contract.withdraw(6, val, {"from": acct})
+    # withdraw_distributor_signtxn = w3.eth.account.sign_transaction(withdraw_distributor_txn, private_key = acct.private_key)
+    # w3.eth.send_raw_transaction(withdraw_distributor_signtxn.rawTransaction)
     return
 
--- scripts/compound.py
+++ scripts/compound.py
@@ -12,37 +12,37 @@ def c_deposit(w3, acct, val):
     nonce = w3.eth.get_transaction_count(acct.address)
     cWETH_address = '0x46e6b214b524310239732D51387075E0e70970bf'
     cWETH_abi = abi.cWETH_abi
-    cWETH_contract = w3.eth.contract(address = cWETH_address, abi = cWETH_abi)
+    cWETH_contract = Contract.from_abi("CometExt", address = cWETH_address, abi = cWETH_abi)
     base_bulk_address = '0x78D0677032A35c63D142a48A2037048871212a8C'
     allowed = True
-    allow_cWETH_txn = cWETH_contract.functions.allow(base_bulk_address, allowed).build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce,})
-    allow_cWETH_signtxn = w3.eth.account.sign_transaction(allow_cWETH_txn, private_key = acct.private_key)
-    w3.eth.send_raw_transaction(allow_cWETH_signtxn.rawTransaction)
+    cWETH_contract.allow(base_bulk_address, allowed, {"from": acct})
+    # allow_cWETH_signtxn = w3.eth.account.sign_transaction(allow_cWETH_txn, private_key = acct.private_key)
+    # w3.eth.send_raw_transaction(allow_cWETH_signtxn.rawTransaction)
     base_bulk_abi = abi.compound_base_bulk_abi
-    base_bulk_contract = w3.eth.contract(address = base_bulk_address, abi = base_bulk_abi)
+    base_bulk_contract = Contract.from_abi("BaseBulker", address = base_bulk_address, abi = base_bulk_abi)
     deposit_action = '0x414354494f4e5f535550504c595f4e41544956455f544f4b454e000000000000'
     data =  '0x00000000000000000000000046e6b214b524310239732d51387075e0e70970bf0000000000000000000000000f25496cf87be88c0a352d822c4ba92479f536010000000000000000000000000000000000000000000000000001c6bf52634000'
-    invoke_bulker_txn = base_bulk_contract.functions.invoke(deposit_action, data).build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce,})
-    invoke_bulker_signtxn = w3.eth.account.sign_transaction(invoke_bulker_txn, private_key = acct.private_key)
-    w3.eth.send_raw_transaction(invoke_bulker_signtxn.rawTransaction)
+    base_bulk_contract.invoke(deposit_action, data, {"from": acct, "value": acct.balance/2})
+    # invoke_bulker_signtxn = w3.eth.account.sign_transaction(invoke_bulker_txn, private_key = acct.private_key)
+    # w3.eth.send_raw_transaction(invoke_bulker_signtxn.rawTransaction)
     return
 
 def c_get_val(w3, acct):
     implement_contract_address = '0x46e6b214b524310239732D51387075E0e70970bf'
-    implement_contract_abi = abi.compound_implement_abi
-    implement_contract = w3.eth.contract(address = implement_contract_address, abi = implement_contract_abi)
-    val = implement_contract.functions.balanceOf(acct.address).call() # how to put val into data?
+    implement_contract_abi = abi.compound_implement_abi 
+    implement_contract = Contract.from_abi("BaseBulker", implement_contract_address, implement_contract_abi)
+    val = implement_contract.balanceOf.call(acct.address, {"from": acct}) # how to put val into data?
     return val
 
 def c_withdraw(w3, acct):
     chainId=1337
     base_bulk_address = '0x78D0677032A35c63D142a48A2037048871212a8C'
     base_bulk_abi = abi.compound_base_bulk_abi
-    base_bulk_contract = w3.eth.contract(address = base_bulk_address, abi = base_bulk_abi)
+    base_bulk_contract = Contract.from_abi("BaseBulker", address = base_bulk_address, abi = base_bulk_abi)
     val = c_get_val(w3, acct)
     withdraw_action = b'0x414354494f4e5f57495448445241575f4e41544956455f544f4b454e00000000'.ljust(32, b'\0')[:32]
     data = b'0x00000000000000000000000046e6b214b524310239732d51387075e0e70970bf0000000000000000000000000f25496cf87be88c0a352d822c4ba92479f53601ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
-    invoke_bulker_txn = base_bulk_contract.functions.invoke(withdraw_action, data).build_transaction({'gas': 200000, 'gasPrice': w3.toWei('0.01', 'gwei'), 'nonce': nonce,})
-    invoke_bulker_signtxn = w3.eth.account.sign_transaction(invoke_bulker_txn, private_key = acct.private_key)
-    w3.eth.send_raw_transaction(invoke_bulker_signtxn.rawTransaction)
+    base_bulk_contract.invoke(withdraw_action, data, {"from": acct, "value": val})
+    # invoke_bulker_signtxn = w3.eth.account.sign_transaction(invoke_bulker_txn, private_key = acct.private_key)
+    # w3.eth.send_raw_transaction(invoke_bulker_signtxn.rawTransaction)
     return
