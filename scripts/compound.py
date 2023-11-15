aa5c6d2ce6cf60b629a5581b414f93021b807a2f
Hanting Li <hanting6@gmail.com>
2023-11-15 14:58:25 -0800

    error with decoded value

--- scripts/compound.py
+++ scripts/compound.py
@@ -1,8 +1,9 @@
 from web3 import Web3, HTTPProvider
 from dotenv import load_dotenv
 from brownie import *
-from eth_abi import encode
+from eth_abi import encode, decode
 from . import abi
+import time
 load_dotenv()
 
 def c_deposit(acct, val):
@@ -12,13 +13,27 @@ def c_deposit(acct, val):
     cWETH_contract = Contract.from_abi("CometExt", address = cWETH_address, abi = cWETH_abi)
     base_bulk_address = '0x78D0677032A35c63D142a48A2037048871212a8C'
     allowed = True
+    cWETH_contract.approve(base_bulk_address, 0, {"from": acct, "gasPrice": 1000000000000, "gas": 10000000, "allow_revert": True})
     cWETH_contract.allow(base_bulk_address, allowed, {"from": acct})
+    cbeth_token_address = '0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22'
+    cbeth_token_abi = abi.alienbase_cbeth_token_abi
+    cbeth_token_contract = Contract.from_abi("UpgradeableOptimismMintableERC20", address = cbeth_token_address, abi = cbeth_token_abi)
+    uniswap_router_address = '0x8c1A3cF8f83074169FE5D7aD50B978e1cD6b37c7'
+    unlim = 115792089237316195423570985008687907853269984665640564039457584007913129639935
+    cbeth_token_contract.approve(uniswap_router_address, unlim, {'from': acct})
+    uniswap_router_abi = abi.alienbase_uniswap_router_abi
+    uniswap_router_contract = Contract.from_abi("UniswapV2Router02", address = uniswap_router_address, abi = uniswap_router_abi)
+    acct_address = acct.address
+    uniswap_router_contract.swapExactETHForTokens(0, ["0x4200000000000000000000000000000000000006", cbeth_token_address], acct_address, int(time.time()) + 60 * 10, {'from':acct, 'value':acct.balance()/5})
     base_bulk_abi = abi.compound_base_bulk_abi
     base_bulk_contract = Contract.from_abi("BaseBulker", address = base_bulk_address, abi = base_bulk_abi)
-    deposit_action = [Web3.toBytes(hexstr='0x414354494f4e5f535550504c595f4e41544956455f544f4b454e000000000000')]
-    val = acct.balance()/2
-    data = [encode(['address', 'address', 'address', 'uint'], [cWETH_address, acct.address, cbeth_token_address, int(val)]).hex()]
-    base_bulk_contract.invoke(deposit_action, data, {"from": acct, "gasPrice": 100000000000000000, "gas": 10000000,})
+    deposit_action = [Web3.toBytes(hexstr='0x414354494f4e5f535550504c595f415353455400000000000000000000000000')]
+    val = int(acct.balance()/7)
+    print(val, acct.balance())
+    data = [encode(['address', 'address', 'address', 'uint'], [cWETH_address, acct.address, cbeth_token_address, val])]
+    print(decode(['address', 'address', 'address', 'uint256'], data[0]))
+    print(cbeth_token_contract.balanceOf.call(acct.address, {"from": acct}), val, acct.balance())
+    base_bulk_contract.invoke(deposit_action, data, {"from": acct, "value": val, "gasPrice": 100000000000000000, "gas": 10000000, "allow_revert": True})
     print([i for i in history])
     return val
 
