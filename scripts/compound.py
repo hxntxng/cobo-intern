bd4a453dff665804f7577bf385e99b78cfa72b03
Hanting Li <hanting6@gmail.com>
2023-11-21 22:57:56 -0800

    debugged invoke function but still no get_val result

--- scripts/compound.py
+++ scripts/compound.py
@@ -13,9 +13,8 @@ def c_deposit(acct, val):
     cWETH_contract = Contract.from_abi("CometExt", address = cWETH_address, abi = cWETH_abi)
     base_bulk_address = '0x78D0677032A35c63D142a48A2037048871212a8C'
     allowed = True
-    cWETH_contract.approve(base_bulk_address, 0, {"from": acct, "gasPrice": 1000000000000, "gas": 10000000, "allow_revert": True})
-    cWETH_contract.allow(base_bulk_address, allowed, {"from": acct})
-    cbeth_token_address = '0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22'
+    # cWETH_contract.allow(base_bulk_address, allowed, {"from": acct})
+    # cbeth_token_address = '0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22'
     cbeth_token_abi = abi.alienbase_cbeth_token_abi
     cbeth_token_contract = Contract.from_abi("UpgradeableOptimismMintableERC20", address = cbeth_token_address, abi = cbeth_token_abi)
     uniswap_router_address = '0x8c1A3cF8f83074169FE5D7aD50B978e1cD6b37c7'
@@ -28,11 +27,12 @@ def c_deposit(acct, val):
     base_bulk_abi = abi.compound_base_bulk_abi
     base_bulk_contract = Contract.from_abi("BaseBulker", address = base_bulk_address, abi = base_bulk_abi)
     deposit_action = [Web3.toBytes(hexstr='0x414354494f4e5f535550504c595f415353455400000000000000000000000000')]
-    val = int(acct.balance()/7)
+    val = int(acct.balance()/70)
     print(val, acct.balance())
     data = [encode(['address', 'address', 'address', 'uint'], [cWETH_address, acct.address, cbeth_token_address, val])]
     print(decode(['address', 'address', 'address', 'uint256'], data[0]))
     print(cbeth_token_contract.balanceOf.call(acct.address, {"from": acct}), val, acct.balance())
+    cbeth_token_contract.approve(cWETH_address, 1000*10**18, {"from": acct, "gasPrice": 1000000000000, "gas": 10000000, "allow_revert": True})
     base_bulk_contract.invoke(deposit_action, data, {"from": acct, "value": val, "gasPrice": 100000000000000000, "gas": 10000000, "allow_revert": True})
     print([i for i in history])
     return val
